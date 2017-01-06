e148f5b1bd4151d6376fe9ae173d18f7a51e6dbd
Leland Krych <leland.krych@gmail.com>
2017-01-06 20:08:56 +0000

    Add session helpers and integration test for login

--- oreja/spec/controllers/sessions_controller_spec.rb
+++ oreja/spec/controllers/sessions_controller_spec.rb
@@ -7,4 +7,17 @@ RSpec.describe "Test Sessions", :type => :request do
     get login_path
     expect(response).to be_success
   end
+  
+  it "renders the right template during login" do
+      get login_path
+      expect(response).to render_template('sessions/new')
+    end
+    
+  it "handles an invalid login" do
+      post login_path, params: { session: { email: "", password: "" } }
+      expect(response).to render_template('sessions/new')
+      expect(flash[:danger]).to be_present
+      get signup_path
+      expect(flash[:danger]).to_not be_present
+  end
 end
